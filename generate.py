bef9547839bd64898fd4e53d0d46238593b9b670
Govardhan <saigov14@gmail.com>
2024-11-09 12:56:49 +0530

    major revamp: blogs, embeds - jinja

--- generate.py
+++ generate.py
@@ -1,19 +1,50 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
-
-# Load the data
-with open('data.yaml', 'r') as file:
-    data = yaml.safe_load(file)
+import os
 
 # Set up Jinja2 environment
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('template.html')
 
-# Render the template
-output = template.render(data)
+# All the webpages that need to be generated:
+pages = ['index.html', 'calendar.html', 'blog.html', "cv.html"]
 
-# Write the output to a file
-with open('index.html', 'w') as file:
-    file.write(output)
+for page in pages:
+    # Load the data
+    with open('data.yaml', 'r') as file:
+        data = yaml.safe_load(file)
 
-print("Website generated successfully!")
+    # Load the template
+    template_path = os.path.join('templates', page)
+    template = env.get_template(template_path)
+
+    # Render the template
+    output = template.render(data)
+
+    # Write the output to a file
+    with open(page, 'w') as file:
+        file.write(output)
+
+    print("INFO: Generated", page)
+
+# Generate all the blogs
+
+# Get all the blogs
+blogs = [file for file in os.listdir('blog') if file.endswith('.html')]
+
+for blog in blogs:
+    # Load the data
+    with open('data.yaml', 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Load the template
+    template_path = os.path.join('blog', blog)
+    template = env.get_template(template_path)
+
+    # Render the template
+    output = template.render(data)
+
+    # Write the output to a file
+    with open(blog, 'w') as file:
+        file.write(output)
+
+    print("INFO: Generated", blog)
